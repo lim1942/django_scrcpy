f2138b989ae7ca32d6007d3d4f4e65101f8cb5fb
Your Name <you@example.com>
2023-05-19 01:04:02 +0800

    fix

--- asynch/recorder.py
+++ asynch/recorder.py
@@ -38,5 +38,9 @@ class RecorderTool:
     @classmethod
     async def del_recorder_socket(cls, session_id):
         if session_id in cls.RECORDER_CLIENT_SOCKET:
-            await cls.RECORDER_CLIENT_SOCKET[session_id].disconnect()
+            cls.RECORDER_CLIENT_SOCKET[session_id].writer.write_eof()
+            await cls.RECORDER_CLIENT_SOCKET[session_id].writer.drain()
             del cls.RECORDER_CLIENT_SOCKET[session_id]
+
+
+
