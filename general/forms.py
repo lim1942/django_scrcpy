69e9de01487dfa6e7170681acd8cf526ae31782e
lim <you@example.com>
2023-05-19 17:41:42 +0800

    fix

--- general/forms.py
+++ general/forms.py
@@ -52,8 +52,8 @@ class MobileForm(forms.ModelForm):
             config_dict['audio_codec'] = 'raw'
         else:
             config_dict['audio_codec'] = 'opus'
-        if (not config_dict['recorder_mkv']) and (config_dict['audio_codec']=='opus'):
-            raise forms.ValidationError("mp4 cant use opus audio_codec !!!")
+        if (config_dict['recorder']) and (not config_dict['recorder_mkv']) and (config_dict['audio_codec']!='aac'):
+            raise forms.ValidationError("recording: mp4 audio_codec only support aac!!!")
         # restore config_dict to config str
         self.cleaned_data['config'] = json.dumps(config_dict)
         return self.cleaned_data
